b4b2f0b572e868259036c8e4eb8191f212c7cfc5
bingsuzoa <zxc_777@naver.com>
2025-07-28 13:36:11 +0900

    feat/test : winningLotto 유효성 검사 로직 추가 및 테스트 완료

--- src/test/java/domain/lotto/WinningLottoTest.java
+++ src/test/java/domain/lotto/WinningLottoTest.java
@@ -17,4 +17,23 @@ public class WinningLottoTest {
             winningLotto.getLastWeekWinningLottos().add(5, 7);
         });
     }
+
+    /// //예외 테스트
+    @Test
+    @DisplayName("랜덤 숫자가 1 ~ 45 범위가 아닌 경우 예외 발생")
+    void 랜덤_숫자_범위_확인() {
+        List<Integer> lottoNumbers = List.of(0, 1, 2, 3, 4, 5);
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new WinningLotto(lottoNumbers));
+        Assertions.assertEquals(e.getMessage(), Lotto.INVALID_LOTTO_NUMBERS);
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자 갯수가 허용된 갯수가 아닌 경우 예외 발생")
+    void 랜덤_숫자가_허용된_갯수인지_확인() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new WinningLotto(lottoNumbers));
+        Assertions.assertEquals(e.getMessage(), Lotto.INVALID_LOTTOS_SIZE);
+    }
 }
