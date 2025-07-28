3a042fc1883b2f4938d413941da7123a3d7dbaf5
bingsuzoa <zxc_777@naver.com>
2025-07-28 13:13:31 +0900

    feat : randomNumbers 파라미터로 받을 때 유효성 검사 후 lotto 생성하도록 로직 변경

--- src/main/java/domain/lotto/Lotto.java
+++ src/main/java/domain/lotto/Lotto.java
@@ -8,13 +8,30 @@ import java.util.stream.Collectors;
 public class Lotto {
 
     public Lotto(List<Integer> randomNumbers) {
-        this.lottoNumbers = List.copyOf(randomNumbers);
+        isValidateRandomNumbers(randomNumbers);
+        this.lottoNumbers = randomNumbers;
     }
 
-    public static final int LottoPrice = 1000;
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final String INVALID_LOTTO_NUMBERS = "로또 숫자 범위가 유효하지 않습니다.";
+    public static final String INVALID_LOTTOS_SIZE = "로또 숫자 갯수가 유효하지 않습니다.";
     private final List<Integer> lottoNumbers;
 
-
+    private void isValidateRandomNumbers(List<Integer> randomNumbers) {
+        Set<Integer> uniqueSet = new HashSet<>(randomNumbers);
+        for(int lottoNumber : randomNumbers) {
+            if(lottoNumber < MIN_LOTTO_NUMBER || lottoNumber > MAX_LOTTO_NUMBER) {
+                throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS);
+            }
+        }
+        if(uniqueSet.size() != LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException(INVALID_LOTTOS_SIZE);
+        }
+    }
+    
     public List<Integer> getLottoNumbers() {
         return lottoNumbers;
     }
@@ -34,7 +51,7 @@ public class Lotto {
         List<Integer> winningNumbers = winningLotto.getLastWeekWinningLottos();
 
         Set<Integer> compareSet = new HashSet<>();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < LOTTO_NUMBERS_COUNT; i++) {
             compareSet.add(lottoNumbers.get(i));
             compareSet.add(winningNumbers.get(i));
         }
