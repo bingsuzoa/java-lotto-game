c3f77e2b2d3ed0774f7531322a0e3bb343ea8c99
bingsuzoa <zxc_777@naver.com>
2025-07-28 12:49:22 +0900

    refactor: LottoController에 Money 적용

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import domain.lottoMachine.PurchaseResult;
+import domain.money.Money;
 import service.LottoService;
 import view.InputVIew;
 import view.OutputView;
@@ -12,33 +13,29 @@ public class LottoController {
     }
 
     private final LottoService lottoService;
+    private static final Money INSUFFICENT_MONEY = null;
 
     public void startGame() {
-        PurchaseResult purchaseResult;
-        do {
-            purchaseResult = purchaseLottos(getMoney());
-        } while(purchaseResult == null);
+        PurchaseResult purchaseResult = lottoService.purchaseLottos(getPurchasbleMoney());
         OutputView.printLottos(purchaseResult);
         OutputView.printResult(lottoService.getResultStatistics(purchaseResult.lottos(), getLatestWinningNumbers()));
     }
 
-    private PurchaseResult purchaseLottos(int money) {
-        PurchaseResult purchaseResult;
-        try {
-            purchaseResult = lottoService.purchaseLottos(money);
-            return purchaseResult;
-        } catch (IllegalArgumentException e) {
-            OutputView.printMessage(e.getMessage());
-            return null;
-        }
+    private Money getPurchasbleMoney() {
+        Money money;
+        do {
+            money = inputPurchasableMoney();
+        } while (money == INSUFFICENT_MONEY);
+        return money;
     }
 
-    private int getMoney() {
-        int money;
-        do {
-            money = InputVIew.getMoney();
-        } while (money == -1);
-        return money;
+    private Money inputPurchasableMoney() {
+        try {
+            return new Money(InputVIew.getMoney());
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            return INSUFFICENT_MONEY;
+        }
     }
 
     private String getLatestWinningNumbers() {
