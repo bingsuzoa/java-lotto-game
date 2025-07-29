3340a30865da46422845e17a326c59b90512d435
bingsuzoa <zxc_777@naver.com>
2025-07-29 12:21:07 +0900

    test : 로또 구매할 수 있는 최소 금액 확인 테스트

--- src/test/java/service/LottoServiceTest.java
+++ src/test/java/service/LottoServiceTest.java
@@ -1,9 +1,12 @@
 package service;
 
+import domain.lotto.lottoPricePolicy.FixedLottoPricePolicy;
+import domain.lotto.lottoPricePolicy.LottoPricePolicy;
 import domain.lottoMachine.LottoMachine;
 import domain.lottoMachine.PurchaseResult;
 import domain.lottoNumberGenerator.LottoNumberGenerator;
 import domain.lottoNumberGenerator.RandomNumberGenerator;
+import domain.money.Money;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,13 +14,13 @@ import org.junit.jupiter.api.Test;
 public class LottoServiceTest {
 
     private LottoNumberGenerator lottoNumberGenerator = new RandomNumberGenerator();
-    private LottoService lottoService = new LottoService(lottoNumberGenerator);
+    private LottoPricePolicy lottoPricePolicy = new FixedLottoPricePolicy();
+    private LottoService lottoService = new LottoService(lottoNumberGenerator, lottoPricePolicy);
 
     @Test
     @DisplayName("돈을 주면 로또와 거스름돈을 주는지 확인")
     void 로또_구매() {
-        int money = 14500;
-        PurchaseResult purchaseResult = lottoService.purchaseLottos(money);
+        PurchaseResult purchaseResult = lottoService.purchaseLottos(new Money(14500));
         Assertions.assertEquals(purchaseResult.getLottoCount(), 14);
         Assertions.assertEquals(purchaseResult.change(), 500);
     }
@@ -29,8 +32,8 @@ public class LottoServiceTest {
     void 로또_구매_최소_금액_미달시_예외() {
         int money = 900;
         IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            lottoService.purchaseLottos(money);
+            lottoService.validateSufficientMoney(new Money(900));
         });
-        Assertions.assertEquals(e.getMessage(), LottoMachine.INSUFFICIENT_PAYMENT_MESSAGE);
+        Assertions.assertEquals(e.getMessage(), LottoMachine.INSUFFICIENT_LOTTO_PRICE);
     }
 }
