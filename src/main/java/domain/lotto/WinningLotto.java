a3e2137b5643c7769a578820edfcd3064c7129be
bingsuzoa <zxc_777@naver.com>
2025-07-29 11:00:25 +0900

    feat : 문자열을 받아 WinningLotto 생성하는 로직으로 변경

--- src/main/java/domain/lotto/WinningLotto.java
+++ src/main/java/domain/lotto/WinningLotto.java
@@ -1,30 +1,51 @@
 package domain.lotto;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import static domain.lotto.Lotto.*;
 
 public class WinningLotto {
 
-    public WinningLotto(List<Integer> randomNumbers) {
-        this.winningNumbers = List.copyOf(randomNumbers);
+    public WinningLotto(String lastWinnings) {
+        List<Integer> lastWeekWinningNumbers = parseLastWeekWinningNumbers(lastWinnings);
+        validateRandomNumbers(lastWeekWinningNumbers);
+        winningLottos = List.copyOf(lastWeekWinningNumbers);
     }
 
-    private final List<Integer> winningNumbers;
+    private final static int WINNING_LOTTO_NUMBERS_COUNT = 6;
+    private final List<Integer> winningLottos;
 
-    public static WinningLotto ofLastWeekWinningNumbers(String lastWinnings) {
-        List<Integer> randomNumbers = parseLastWeekWinningNumbers(lastWinnings);
-        WinningLotto winningLotto = new WinningLotto(randomNumbers);
-        return winningLotto;
+    private void validateRandomNumbers(List<Integer> randomNumbers) {
+        validateUniqueNumbers(randomNumbers);
+        validateNumberRange(randomNumbers);
     }
 
-    private static List<Integer> parseLastWeekWinningNumbers(String lastWinnings) {
+    private void validateUniqueNumbers(List<Integer> randomNumbers) {
+        Set<Integer> uniqueSet = new HashSet<>(randomNumbers);
+        if (uniqueSet.size() != WINNING_LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException(INVALID_LOTTOS_SIZE);
+        }
+    }
+
+    private void validateNumberRange(List<Integer> randomNumbers) {
+        for (int lottoNumber : randomNumbers) {
+            if (lottoNumber < MIN_LOTTO_NUMBER || lottoNumber > MAX_LOTTO_NUMBER) {
+                throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS);
+            }
+        }
+    }
+
+    private List<Integer> parseLastWeekWinningNumbers(String lastWinnings) {
         return Arrays.stream(lastWinnings.replace(" ", "").split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
 
     public List<Integer> getLastWeekWinningLottos() {
-        return winningNumbers;
+        return winningLottos;
     }
 }
