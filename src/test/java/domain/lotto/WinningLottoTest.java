9045669eecaedee2e156cec2bce47b5bf7ee0354
bingsuzoa <zxc_777@naver.com>
2025-07-29 11:01:26 +0900

    refactor : winningLotto 설계 변경됨에 따라 test 구조 수정

--- src/test/java/domain/lotto/WinningLottoTest.java
+++ src/test/java/domain/lotto/WinningLottoTest.java
@@ -11,7 +11,7 @@ public class WinningLottoTest {
     @Test
     @DisplayName("정답 번호 외부에서 수정 불가한지 확인")
     void 수정_불가_확인() {
-        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        String lottoNumbers = "1,2,3,4,5,6";
         WinningLotto winningLotto = new WinningLotto(lottoNumbers);
         Assertions.assertThrows(UnsupportedOperationException.class, () -> {
             winningLotto.getLastWeekWinningLottos().add(5, 7);
@@ -22,7 +22,7 @@ public class WinningLottoTest {
     @Test
     @DisplayName("랜덤 숫자가 1 ~ 45 범위가 아닌 경우 예외 발생")
     void 랜덤_숫자_범위_확인() {
-        List<Integer> lottoNumbers = List.of(0, 1, 2, 3, 4, 5);
+        String lottoNumbers = "1,2,3,4,5,67";
         IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () ->
                 new WinningLotto(lottoNumbers));
         Assertions.assertEquals(e.getMessage(), Lotto.INVALID_LOTTO_NUMBERS);
@@ -31,7 +31,7 @@ public class WinningLottoTest {
     @Test
     @DisplayName("랜덤 숫자 갯수가 허용된 갯수가 아닌 경우 예외 발생")
     void 랜덤_숫자가_허용된_갯수인지_확인() {
-        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        String lottoNumbers = "1,2,3,4,5,7,8";
         IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, () ->
                 new WinningLotto(lottoNumbers));
         Assertions.assertEquals(e.getMessage(), Lotto.INVALID_LOTTOS_SIZE);
