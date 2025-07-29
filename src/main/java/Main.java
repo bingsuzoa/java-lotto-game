9ccfdd92ed25072a285f32f514ad9b154e91f8f1
bingsuzoa <zxc_777@naver.com>
2025-07-29 12:40:04 +0900

    feat : Main에서 가격 정책 지정 후 주입

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import controller.LottoController;
+import domain.lotto.lottoPricePolicy.FixedLottoPricePolicy;
+import domain.lotto.lottoPricePolicy.LottoPricePolicy;
 import domain.lottoNumberGenerator.LottoNumberGenerator;
 import domain.lottoNumberGenerator.RandomNumberGenerator;
 import service.LottoService;
@@ -8,7 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
         LottoNumberGenerator numberGenerator = new RandomNumberGenerator();
-        LottoService lottoService = new LottoService(numberGenerator);
+        LottoPricePolicy lottoPricePolicy = new FixedLottoPricePolicy();
+        LottoService lottoService = new LottoService(numberGenerator, lottoPricePolicy);
         LottoController lottoController = new LottoController(lottoService);
         lottoController.startGame();
     }
